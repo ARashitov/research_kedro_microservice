e66e49493809daf42bb8d6ae4740ba1cd3d46db4
AtmosOne <adil.rashitov.98@gmail.com>
2022-05-05 21:11:58 +0600

    feat(backend): add endpoint log records schema

---
+++ src/backend/schemas/logs.py
@@ -0,0 +1,24 @@
+from typing import Any, Union
+from pydantic import BaseModel
+
+
+class EndpointLogRecord(BaseModel):
+    """Schema used to capture endpoint call details"""
+
+    app_environment: str
+    app_version: str
+
+    request_uri: str
+    request_method: str
+    request_path: str
+    request_size: int
+    request_datetime: str
+    request_headers: Any
+
+    response_status_code: int
+    response_size: int
+    response_datetime: str
+    response_duration: Union[float, str]
+    response_headers: Any
+
+    exception_message: Union[str, None]
