f90d1aee3bfbd219929b67f0fdb00438deea420e
Adil <32989359+AtmosOne@users.noreply.github.com>
2022-06-09 16:22:27 +0600

    feat(infrastructure): setup vpc

---
+++ terraform/shared/README.md
@@ -0,0 +1,4 @@
+# shared
+
+Modules need to be deployed only under `shared` workspace
+`export TF_WORKSPACE=shared`
---
+++ terraform/shared/vpc/variables.tf
@@ -0,0 +1,36 @@
+variable "tags" {
+  default = {
+    project_name = "research-kedro-microservice"
+    region       = "us-east-2"
+  }
+  description = "Tags set to VPC"
+  type        = map(string)
+}
+
+variable "amt_asg_subnets_per_env" {
+  type        = number
+  default     = 2
+  description = "Amount of subnets for each autoscaling service under project environments"
+}
+
+variable "amt_aws_batch_subnets_per_env" {
+  type        = number
+  default     = 2
+  description = "Amount of subnets for each aws batch service under project environments"
+}
+
+variable "amt_reserved_subnets_per_env" {
+  type        = number
+  default     = 1
+  description = "Amount of reserved subnets under project environments"
+}
+
+variable "environments" {
+  type = list(string)
+  default = [
+    "test",
+    "ua_test",
+    "prod",
+  ]
+  description = "Environments used in project"
+}
