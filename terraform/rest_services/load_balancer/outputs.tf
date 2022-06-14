e6caaf82dd7e5feb0e818bbd0f427ae1d0b43b98
Adil <32989359+AtmosOne@users.noreply.github.com>
2022-06-14 12:11:43 +0600

    feat(infrastructure): activate outputs of load balancer for autoscaling group

--- terraform/rest_services/load_balancer/outputs.tf
+++ terraform/rest_services/load_balancer/outputs.tf
@@ -1,20 +1,20 @@
-# output "lb_id" {
-#   value       = module.alb.lb_id
-#   description = "The ID and ARN of the load balancer we created."
-# }
+output "lb_id" {
+  value       = module.alb.lb_id
+  description = "The ID and ARN of the load balancer we created."
+}
 
-# output "target_group_arns" {
-#   value       = module.alb.target_group_arns
-#   description = "ARNs of the target groups. Useful for passing to your Auto Scaling group."
-# }
+output "target_group_arns" {
+  value       = module.alb.target_group_arns
+  description = "ARNs of the target groups. Useful for passing to your Auto Scaling group."
+}
 
-# output "lb_dns_name" {
-#   value = {
-#     # stable = "${module.alb.lb_dns_name}:8000"
-#     latest = "${module.alb.lb_dns_name}:8001"
-#   }
-#   description = "The DNS name of the load balancer."
-# }
+output "lb_dns_name" {
+  value = {
+    # stable = "${module.alb.lb_dns_name}:8000"
+    latest = "${module.alb.lb_dns_name}:80"
+  }
+  description = "The DNS name of the load balancer."
+}
 
 output "env" {
   value       = local.env
