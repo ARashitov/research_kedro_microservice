e1ced0dc0fac4ad7ccece1f5e96297421eb8f55b
AtmosOne <adil.rashitov.98@gmail.com>
2022-06-17 20:05:31 +0600

    test(pipelines): add catalog & pipeline fixtures

--- src/tests/conftest.py
+++ src/tests/conftest.py
@@ -7,8 +7,11 @@ from kedro.extras.extensions.ipython import _find_kedro_project
 from kedro.framework.context.context import KedroContext
 from kedro.framework.session import KedroSession
 from kedro.framework.startup import bootstrap_project
+from kedro.io import DataCatalog
+from kedro.pipeline import Pipeline
 import pytest
 
+from src.pipelines.pipeline_registry import register_pipelines
 
 ENVIRONMENT = "test"
 
@@ -34,3 +37,21 @@ def context() -> KedroContext:
     _context = session.load_context()
 
     return _context
+
+
+@pytest.fixture
+def catalog(context: KedroContext) -> DataCatalog:
+    """Function performs extraction of kedro context using built-in tools
+
+    Args:
+        env (str): input environment
+
+    Returns:
+        KedroContext: Generated kedro context
+    """
+    return context.catalog
+
+
+@pytest.fixture
+def pipelines() -> Pipeline:
+    return register_pipelines()
