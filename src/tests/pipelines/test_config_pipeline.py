d443063e695e702fa0016648f382ae31f1028f5a
AtmosOne <adil.rashitov.98@gmail.com>
2022-06-17 20:33:41 +0600

    fix(pipelines): patch task_id env var before execution test_config pipeline

--- src/tests/pipelines/test_config_pipeline.py
+++ src/tests/pipelines/test_config_pipeline.py
@@ -2,19 +2,27 @@
 This is a boilerplate pipeline 'example'
 generated using Kedro 0.18.0
 """
+import os
 from kedro.framework.context.context import KedroContext
 from kedro.runner import SequentialRunner
 
 from src.pipelines.pipelines import config_pipeline
 
 
-def test_config_pipeline(context: KedroContext):
+class TestConfigPipeline:
+    def __patch_task_id_env_var(self):
+        # TODO: Mock this module in future
+        os.environ["task_id"] = "TEST_TASK_ID"
 
-    runner = SequentialRunner()
-    result = runner.run(
-        config_pipeline.create_pipeline(),
-        catalog=context.catalog,
-    )
+    def test_config_pipeline(self, context: KedroContext):
 
-    assert "params:common.task_id" in result.keys()
-    assert "params:credentials.google_maps_key" in result.keys()
+        self.__patch_task_id_env_var()
+
+        runner = SequentialRunner()
+        result = runner.run(
+            config_pipeline.create_pipeline(),
+            catalog=context.catalog,
+        )
+
+        assert "params:common.task_id" in result.keys()
+        assert "params:credentials.google_maps_key" in result.keys()
